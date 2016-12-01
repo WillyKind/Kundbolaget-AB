75837f3bd9b8824ce7c1d65de3d1b0ce9efd2537
Robert Andersson <inputfalken@gmail.com>
2016-12-01 15:23:54 +0100

    Adds DbSets for model

--- Kundbolaget/Models/Context/StoreContext.cs
+++ Kundbolaget/Models/Context/StoreContext.cs
@@ -8,6 +8,9 @@ namespace Kundbolaget.Models.Context
 {
     public class StoreContext : DbContext
     {
-        
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Container> Containers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductGroup> ProductGroups { get; set; }
     }
 }

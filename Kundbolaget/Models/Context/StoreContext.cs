cd317475fe050372e51e59b3a3e6502cfe6eecab
Robert Andersson <inputfalken@gmail.com>
2016-12-01 11:39:14 +0100

    Adds StoreContext for entity framework.

---
+++ Kundbolaget/Models/Context/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Kundbolaget.Models.Context
+{
+    public class StoreContext : DbContext
+    {
+        
+    }
+}

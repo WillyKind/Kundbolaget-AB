c18bb09ad79ce67623ab0e3f29c310407164dd76
Michel Miladinovic <miladino77@hotmail.com>
2016-12-01 13:30:25 +0100

    Adds Product to Model

---
+++ Kundbolaget/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kundbolaget.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int ContainerId { get; set; }
+        public int ProductGroupId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Abv { get; set; }
+        public double TradingMargin { get; set; }
+        public double PurchasePrice { get; set; }
+        public bool Removed { get; set; }
+    }
+}

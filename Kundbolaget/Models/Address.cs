9dfe47533d64cc93f2858572049bdaf9d6dd35ba
Michel Miladinovic <miladino77@hotmail.com>
2016-12-01 15:28:10 +0100

    Adds Address to Model

---
+++ Kundbolaget/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kundbolaget.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string ZipCode{ get; set; }
+
+    }
+}

328c8b40362ac945a5ba6e7679cf73b0174af1d1
Johan Wänström <johan.wanstrom@gmail.com>
2016-12-06 13:05:30 +0100

    Adds various folders
    Adds EF-folder, Repositories-folder, Entitymodels-folder, Interfaces-folder
    Adds empty IEntityRepository-interface
    Moved Entitymodels to separate folder EnityModels and moved storecontext to entityframework-folder

--- Kundbolaget/Migrations/Configuration.cs
+++ Kundbolaget/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using Kundbolaget.EntityFramework.Context;
+
 namespace Kundbolaget.Migrations
 {
     using System;
@@ -5,14 +7,14 @@ namespace Kundbolaget.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Kundbolaget.Models.Context.StoreContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<StoreContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Kundbolaget.Models.Context.StoreContext context)
+        protected override void Seed(StoreContext context)
         {
             //  This method will be called after migrating to the latest version.
 

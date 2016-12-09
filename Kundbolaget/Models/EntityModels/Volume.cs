744495a5a3d8fbcf02db17f220a9ea3f74e4b507
Robert Andersson <inputfalken@gmail.com>
2016-12-09 20:26:59 +0100

    Adds migrations which adds a volume entity

--- Kundbolaget/Models/EntityModels/Volume.cs
+++ Kundbolaget/Models/EntityModels/Volume.cs
@@ -3,13 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Kundbolaget.Models.EntityModels
 {
-    public class Container
+    public class Volume
     {
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
-        public virtual ICollection<ProductInfo> ProductsInfoes { get; set; }
+        public int Milliliter { get; set; }
+        public virtual ICollection<ProductInfo> ProductInfos { get; set; }
         [Required]
         public bool IsRemoved { get; set; }
     }

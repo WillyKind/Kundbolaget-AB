7df6f1048ef8f2fdf04f95107760defe2794a9f4
Viktor Gustafsson <viktor.b.gustafsson@gmail.com>
2016-12-19 15:39:24 +0100

    adds orderviewmodel

---
+++ Kundbolaget/ViewModels/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kundbolaget.Models.EntityModels;
+
+namespace Kundbolaget.ViewModels
+{
+    public class OrderViewModel
+    {
+        public Company[] Companies { get; set; }
+        public Order Order { get; set; }     
+    }
+}

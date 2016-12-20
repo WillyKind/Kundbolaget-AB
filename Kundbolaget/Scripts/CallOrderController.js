53b39d21c64df291ac5dcd308458e2652433a5ad
Johan Wänström <johan.wanstrom@gmail.com>
2016-12-20 14:05:19 +0100

    Adds more DRY CallOrderController

---
+++ Kundbolaget/Scripts/CallOrderController.js
@@ -0,0 +1,12 @@
+﻿function OrderController(id, method) {
+    $.ajax({
+        url: "/Order/"+method+"/" + id,
+        type: 'POST',
+        success: function (data) {
+            $("#" + id).fadeOut("slow", function()  {                
+            });
+        }
+
+    });
+}
+

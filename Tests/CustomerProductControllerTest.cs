b00a9345a8b29abf71eeb7fc7e34574327deac04
Willy Kind <willykkind@gmail.com>
2016-12-22 10:42:03 +0100

    adds testclass for CustomerProductController

---
+++ Tests/CustomerProductControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kundbolaget.Controllers;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class CustomerProductControllerTest
+    {
+        private CustomerProductController _customerProductController;
+
+        [SetUp]
+        public void Initializer()
+        {
+            _customerProductController = new CustomerProductController();
+        }
+    }
+}

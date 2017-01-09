67e47526ab32a39d843c24f7fbc78e2207fe0bd2
Michel Miladinovic <miladino77@hotmail.com>
2017-01-09 10:38:30 +0100

    Moves companyinfo to bottom of page & adds delivery date

--- Kundbolaget/PdfGenerator/PdfGenerator.cs
+++ Kundbolaget/PdfGenerator/PdfGenerator.cs
@@ -27,12 +27,9 @@ namespace Kundbolaget.PdfGenerator
                               od.OrderId,
                               p.Name,
                               od.Amount,
-                              od.UnitPrice,
-                              od.TotalPrice,
                               Container = p.Container.Name,
                               p.Volume,
-                              od.ProductInfoId
-                              
+                              od.ProductInfoId          
                           };
 
             var company = from od in db.OrderDetails
@@ -43,21 +40,22 @@ namespace Kundbolaget.PdfGenerator
                               od.Order.Company.Name,
                               od.Order.Company.DeliveryAddress.Street,
                               od.Order.Company.DeliveryAddress.Number,
-                              od.Order.Company.DeliveryAddress.ZipCode
+                              od.Order.Company.DeliveryAddress.ZipCode,
+                              od.Order.WishedDeliveryDate
+                              
                           };
 
-            Document doc = new Document();
+            var doc = new Document();
             
             string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\_Följesedel_o{id}_c{company.FirstOrDefault().Name}{DateTime.Now.ToShortDateString()}.pdf";
 
             FileStream fS = File.Create(path);
             var pb = new PdfContentByte(PdfWriter.GetInstance(doc, fS));
-            //PdfWriter.GetInstance(doc, fS);
             doc.Open();
 
             var fs1 = FontFactory.GetFont(FontFactory.HELVETICA, 20);
             var fs2 = FontFactory.GetFont(FontFactory.HELVETICA, 10);
-            doc.Add(new Paragraph("Följesedel", fs1) /*{ Alignment = Element.ALIGN_CENTER }*/);
+            doc.Add(new Paragraph("Följesedel", fs1));
             doc.Add(new Paragraph("\n"));
 
             foreach (var c in company)
@@ -65,19 +63,19 @@ namespace Kundbolaget.PdfGenerator
                 doc.Add(new Paragraph($"Ordernummer: {c.OrderId} ") { Alignment = Element.ALIGN_LEFT });
                 doc.Add(new Paragraph($"Beställare: {c.Name} ") { Alignment = Element.ALIGN_LEFT });
                 doc.Add(new Paragraph($"Leveransadress: {c.Street} {c.Number} {c.ZipCode} ") { Alignment = Element.ALIGN_LEFT });
+                doc.Add(new Paragraph($"Leveransdatum: {c.WishedDeliveryDate.ToShortDateString()} ") { Alignment = Element.ALIGN_LEFT });
                 break;
             }
 
             doc.Add(new Paragraph("\n\n"));
 
-            PdfPTable table = new PdfPTable(4);
-            table.HorizontalAlignment = 0;
-
-            PdfPCell cell = new PdfPCell(new Phrase("Detaljer"));
-            cell.Colspan = 4;
-            cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+            var table = new PdfPTable(4) {HorizontalAlignment = 0};
+            var cell = new PdfPCell(new Phrase("Detaljer"))
+            {
+                Colspan = 4,
+                HorizontalAlignment = 1 //0=Left, 1=Centre, 2=Right
+            }; 
             table.AddCell(cell);
-
             table.AddCell("Artikelnr");
             table.AddCell("Produkt");
             table.AddCell("Antal");
@@ -90,30 +88,31 @@ namespace Kundbolaget.PdfGenerator
                 table.AddCell($"{item.Amount}");
                 table.AddCell($"{item.Container} {item.Volume.Milliliter}ml");
             }
-
             doc.Add(table);
-            doc.Add(new Paragraph("\n\n"));
-
-            var table2 = new PdfPTable(2);
-            table2.HorizontalAlignment = 0;
-            //table2.TotalWidth = doc.PageSize.Width - doc.LeftMargin;
             
-            List<PdfPCell> cells = new List<PdfPCell>();
-            cells.Add(new PdfPCell(new Phrase("Kundbolaget AB", fs2)) { Border = 0 });
-            cells.Add(new PdfPCell(new Phrase("Innehar F-skattesedel", fs2)) { Border = 0 });
-            cells.Add(new PdfPCell(new Phrase("Industrivägen 32", fs2)) { Border = 0 });
-            cells.Add(new PdfPCell(new Phrase("Epost: info@kundbolaget.se", fs2)) { Border = 0 });
-            cells.Add(new PdfPCell(new Phrase("142 45 Liljeholmen", fs2)) { Border = 0 });
-            cells.Add(new PdfPCell(new Phrase("Hemsida: www.kundbolaget.se", fs2)) { Border = 0 });
-            cells.Add(new PdfPCell(new Phrase("Sverige", fs2)) { Border = 0 });
-            cells.Add(new PdfPCell(new Phrase("Org.nr: 556223-0102", fs2)) { Border = 0 });
+            var table2 = new PdfPTable(2)
+            {
+                HorizontalAlignment = 0,
+                TotalWidth = doc.PageSize.Width - doc.LeftMargin
+            };
+            var cells = new List<PdfPCell>
+            {
+                new PdfPCell(new Phrase("Kundbolaget AB", fs2)) {Border = 0},
+                new PdfPCell(new Phrase("Innehar F-skattesedel", fs2)) {Border = 0},
+                new PdfPCell(new Phrase("Industrivägen 32", fs2)) {Border = 0},
+                new PdfPCell(new Phrase("Epost: info@kundbolaget.se", fs2)) {Border = 0},
+                new PdfPCell(new Phrase("142 45 Liljeholmen", fs2)) {Border = 0},
+                new PdfPCell(new Phrase("Hemsida: www.kundbolaget.se", fs2)) {Border = 0},
+                new PdfPCell(new Phrase("Sverige", fs2)) {Border = 0},
+                new PdfPCell(new Phrase("Org.nr: 556223-0102", fs2)) {Border = 0}
+            };
 
             foreach (var pdfCell in cells)
             {
                 table2.AddCell(pdfCell);
             }
-            //table2.WriteSelectedRows(0, -1, doc.LeftMargin, doc.PageSize.Height - 400, pb.PdfWriter.DirectContent);
-            doc.Add(table2);
+            table2.WriteSelectedRows(0, -1, doc.LeftMargin, doc.BottomMargin + 60, pb.PdfWriter.DirectContent);
+            //doc.Add(table2);
             doc.Close();
         }
     }

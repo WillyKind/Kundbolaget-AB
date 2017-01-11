0e00eb4aa2ef9e0c89c81ae5291a822dcbb69beb
Johan Wänström <johan.wanstrom@gmail.com>
2017-01-11 11:30:09 +0100

    Adds new buttons and new remove for Produktöversikt

---
+++ Kundbolaget/Scripts/CallProductController.js
@@ -0,0 +1,12 @@
+﻿function ProductController(id, method) {
+    console.log(id);
+    $.ajax({
+        url: "/Product/" + method + "/" + id,
+        type: 'POST',
+        success: function (data) {
+            $("#" + id).fadeOut("slow", function () {
+            });
+        }
+
+    });
+}
